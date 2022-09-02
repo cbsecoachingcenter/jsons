465c9531eda0cd2c1648ce217dd8527136870b3e
cbsecoachingcenter <112490773+cbsecoachingcenter@users.noreply.github.com>
2022-09-02 23:58:31 +0530

    Change

--- class_9th_and_10th/class10th/ncertBooks/biology/main.js
+++ class_9th_and_10th/class10th/ncertBooks/biology/main.js
@@ -20,26 +20,32 @@ xhr.onload = function() {
     lists.forEach(function(element) {
 
       let movies = `<div class="col-md-4" id="${element.Ids}" >
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="${element.Thumbnail}" alt="">
-
-            <div class="card-body card-light bg-dark">
-              <h2 class="card-title text-light">CHAPTER : ${element.ChapterNumber}</h2>
-              <h4 class="card-text text-danger">Name : <span class="text-danger">${element.ChapterName}</span></h4>
-              
-              
-               
-              
-              <div class="d-flex justify-content-between align-items-end my-4">
-              
-                <div class="btn-group">
+         <div class="card mb-4 shadow-sm">
+                            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/ncertBooks/class_9th_and_10th/class10th/img/biologyCh${element.ChapterNumber}.jpg" alt="">
                 
-                
-                 <a href="${element.Download}">
-                  <button type="button" class="btn btn-outline-primary" style="width: 100px;">Download</button>
-                  </a>
-                 
-                  
+                            <div class="card-body card-light bg-dark">
+                              <h2 class="card-title text-light">CHAPTER : ${element.ChapterNumber}</h2>
+                              <h4 class="card-text text-danger">Name : <span class="text-danger">${element.ChapterName}</span></h4>
+                              
+                              
+                               
+                              
+                              <div class="d-flex justify-content-between align-items-end my-4">
+                              
+                                <div class="btn-group">
+                                
+                                
+                                <!-- <a href="${element.Download}">
+                                  <button type="button" class="btn btn-outline-primary" style="width: 100px;">Download</button>
+                                  </a>-->
+                                 
+                                  
+                                     <a href="https://ncert.nic.in/ncerts/l/jesc1${element.ChapterNumber}.pdf">
+                                  <button type="button" class="btn btn-outline-primary" style="width: 100px;">Download</button>
+                                  </a>
+                                 
+                                  
+                         
                   
                 </div>
                 <small class="text-muted">${element.Time} ${element.Am_Pm}</small>
