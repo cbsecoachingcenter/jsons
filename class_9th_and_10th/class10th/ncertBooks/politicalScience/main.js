52a7b82dcea5cfa6e4ae32048a7f19e961b54073
cbsecoachingcentre <112490773+cbsecoachingcentre@users.noreply.github.com>
2022-09-05 01:29:25 +0530

    Updated

--- class_9th_and_10th/class10th/ncertBooks/politicalScience/main.js
+++ class_9th_and_10th/class10th/ncertBooks/politicalScience/main.js
@@ -20,28 +20,50 @@ xhr.onload = function() {
     lists.forEach(function(element) {
 
       let movies = `<div class="col-md-4" id="${element.Ids}" >
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="${element.Thumbnail}" alt="">
-
-            <div class="card-body card-light bg-dark">
-              <h2 class="card-title text-light">CHAPTER : ${element.ChapterNumber}</h2>
-              <h4 class="card-text text-danger">Name : <span class="text-danger">${element.ChapterName}</span></h4>
-              
-              
-               
-              
-              <div class="d-flex justify-content-between align-items-end my-4">
-              
-                <div class="btn-group">
-                
-                
-                 <a href="${element.Download}">
-                  <button type="button" class="btn btn-outline-primary" style="width: 100px;">Download</button>
-                  </a>
-                 
-                  
-                  
-                </div>
+         <div class="card mb-4 shadow-sm">
+                     <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/ncertBooks/class_9th_and_10th/class10th/img/politicalScienceCh${element.ChapterNumber}.jpg" alt="">
+         
+                     <div class="card-body card-light bg-dark">
+                       <h2 class="card-title text-light">CHAPTER : ${element.ChapterNumber}</h2>
+                       <h4 class="card-text text-danger">Name : <span class="text-danger">${element.ChapterName}</span></h4>
+                       
+                       
+                        
+                       
+                     <div class="d-flex justify-content-between align-items-start my-2">
+                                       
+                                         <div class="btn-group">
+                                         
+                                         
+                                              <a href="https://ncert.nic.in/textbook/pdf/jess4${element.ChapterNumber}.pdf">
+                                           <button type="button" class="btn btn-outline-primary" style="width: 100px;">Download</button>
+                                           </a>
+                                           
+                                         </div>
+                                                                         </div>
+         
+                                         
+                                         
+                                         
+                                                                       <div class="d-flex justify-content-between align-items-start mb-2">
+         
+                                         
+                                             <div class="btn-group">
+                                         
+                                         
+                                              <a href="${element.ChapterSolution}">
+                                           <button type="button" class="btn btn-outline-primary" style="width: 100px;">Solutions</button>
+                                           </a>
+                                          
+                                          
+                                          
+                                           
+                                         </div>
+                         
+                                         
+                                         
+                                         
+                         
                 <small class="text-muted">${element.Time} ${element.Am_Pm}</small>
               </div>
             </div>
